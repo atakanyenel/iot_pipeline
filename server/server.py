682a1c9a7aa50f5b0a30fcfd61416472892e0b59
root <root@vm-10-155-208-103.cloud.mwn.de>
2017-11-05 23:13:03 +0100

    make api better

--- server/server.py
+++ server/server.py
@@ -9,7 +9,13 @@ def hello():
     res=es.search(index="temperature",size=100, body={"query": {"match_all": {}}})
     return jsonify(res)
 
-
+@app.route("/api/v1/<string:index>/<int:id>")
+def get_doc(index,id):
+	try:
+		res=es.get(index=index, doc_type='doc', id=id)
+		return jsonify(res)
+	except:
+		return "{}"
 if __name__ == "__main__":
 	app.run()
 
