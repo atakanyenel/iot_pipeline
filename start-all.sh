3c47f7d70e21647d8f5c533d9e7686e21acb71c2
root <root@vm-10-155-208-213.cloud.mwn.de>
2017-11-11 02:28:42 +0100

    support for multiple boards

---
+++ start-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+trap "kill 0" EXIT
+#kafka
+/opt/Kafka/kafka_2.10-0.10.0.1/bin/kafka-server-start.sh /opt/Kafka/kafka_2.10-0.10.0.1/config/server.properties &
+#flink
+/root/flink-1.3.2/bin/start-local.sh
+#elasticsearch as user: elasticuser
+runuser -l elasticuser -c /home/elasticuser/elasticsearch-5.6.3/bin/elasticsearch &
+
+#kafka-flink-elasticsearch connector
+mvn -f ./kafka-flink-101/pom.xml exec:java -Dexec.mainClass=com.grallandco.demos.ReadFromKafka &
+
+
+#python server
+/root/IOT/server/venv/bin/python /root/IOT/server/server.py &
+
+wait
